9d1764909ba1a71b9de9d316f95be5790d3a5d6f
charl2on381 <41657722+charl2on381@users.noreply.github.com>
2023-08-01 22:17:03 +0800

    Update

--- js/modal.js
+++ js/modal.js
@@ -1,19 +1,58 @@
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector("[data-modal-open]"),
-//     closeModalBtn: document.querySelector("[data-modal-close]"),
-//     modal: document.querySelector("[data-modal]"),
-//   };
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
 
-//   refs.openModalBtn.addEventListener("click", toggleModal);
-//   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
-//   function toggleModal() {
-//     refs.modal.classList.toggle("active");
-//   }
-// })();
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
 
-function toggleModal() {
-  document.getElementById("modal").classList.toggle("active");
-}
+// function toggleModal() {
+//   document.getElementById("modal").classList.toggle("active");
+// }
+
+
+// const openModalButtons = document.querySelectorAll('[data-modal-target]')
+// const closeModalButtons = document.querySelectorAll('[data-close-button]')
+// const overlay = document.getElementById('overlay')
+
+
+// openModalButtons.foreach(button => {
+//   button.AddEventListener('click', () => {
+//     const modal = document.querySelector(button.dataset.modalTarget)
+//     openModal(modal)
+//   })
+// })
+
+// overlay.addEventListener('click', () => {
+//   const modals = document.querySelectorAll('.modal.active')
+//   modals.foreach(modal => {
+//     closeModal(modal)
+//   })
+// })
+
+// closeModalButtons.foreach(button => {
+//   button.AddEventListener('click', () => {
+//     const modal = button.closest('.modal')
+//     closeModal(modal)
+//   })
+// })
+
+// function openModal(modal) {
+//   if (modal == null) return
+//   modal.classList.add('active')
+//   overlay.classList.add('active')
+// }
+
+// function closeModal(modal) {
+//   if (modal == null) return
+//   modal.classList.remove('active')
+//   overlay.classList.remove('active')
+// }
 
